3b7ba2b6ae4a5bc33f3e6f52a9d9a2801ed4f3d7
felipy <felipy81br@gmail.com>
2026-01-12 11:28:29 -0300

    chore: create a paste for add-client on client-adm

--- src/modules/client-adm/usecase/add-client/add-client.usecase.ts
+++ src/modules/client-adm/usecase/add-client/add-client.usecase.ts
@@ -1,6 +1,6 @@
-import Id from "../../@shared/domain/value-object/id.value-object";
-import Client from "../domain/client.entity";
-import ClientGateway from "../gateway/client.gateway";
+import Id from "../../../@shared/domain/value-object/id.value-object";
+import Client from "../../domain/client.entity";
+import ClientGateway from "../../gateway/client.gateway";
 import { AddClientInputDto, AddClientOutputDto } from "./add-client.dto";
 
 
