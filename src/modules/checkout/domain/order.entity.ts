aaa4b45323179d58a6afb5dcef2406b6229d7ac7
felipy <felipy81br@gmail.com>
2026-01-24 14:54:09 -0300

    feat: add created and updated to entity order and product

--- src/modules/checkout/domain/order.entity.ts
+++ src/modules/checkout/domain/order.entity.ts
@@ -1,3 +1,4 @@
+import AggregateRoot from "../../@shared/domain/entity/aggregate-root.interface";
 import BaseEntity from "../../@shared/domain/entity/base.entity";
 import Id from "../../@shared/domain/value-object/id.value-object";
 import Client from "./client.entity";
@@ -8,15 +9,17 @@ type OrderProps = {
   client: Client;
   products: Product[];
   status?: string;
+  createdAt?: Date
+  updatedAt?: Date
 };
 
-export default class Order extends BaseEntity {
+export default class Order extends BaseEntity implements AggregateRoot {
   private _client: Client;
   private _products: Product[];
   private _status: string;
 
   constructor(props: OrderProps) {
-    super(props.id);
+    super(props.id, props?.createdAt, props?.updatedAt);
     this._client = props.client;
     this._products = props.products;
     this._status = props.status || "pending";
