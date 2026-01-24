d5edc460439339265bf1a24cb7b74b3faf34c8aa
felipy <felipy81br@gmail.com>
2026-01-24 15:08:49 -0300

    feat: add product persistence model

---
+++ src/modules/checkout/repository/product/product.model.ts
@@ -0,0 +1,27 @@
+import { Column, Model, PrimaryKey, Table } from "sequelize-typescript";
+
+@Table({
+  tableName: "products",
+  timestamps: false,
+})
+export class ProductModel extends Model {
+
+  @PrimaryKey
+  @Column({ allowNull: false })
+  id: string;
+
+  @Column({ allowNull: false })
+  name: string;
+
+  @Column({ allowNull: false })
+  description: string;
+
+  @Column({ allowNull: false })
+  salesPrice: number;
+
+  @Column({ allowNull: false, field: 'created_at' })
+  createdAt: Date;
+
+  @Column({ allowNull: false, field: 'updated_at' })
+  updatedAt: Date;
+}
